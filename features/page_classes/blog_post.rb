9c5503e1fc87ae83e68aafef8ff4807938cba80a
Cooper Legeza <cooperlegeza@gmail.com>
2016-12-08 15:32:43 -0500

    Initial commit

---
+++ features/page_classes/blog_post.rb
@@ -0,0 +1,30 @@
+require 'page-object'
+require 'watir-webdriver'
+
+class BlogPost
+  include PageObject
+
+  span(:title, :id => 'title')
+  span(:author, :id => 'author')
+  div(:text, :id => 'text')
+  span(:date, :id => 'date-created')
+  text_field(:comment_author, :id => 'authorId')
+  text_area(:comment_text, :id => 'commentId')
+  span(:first_comment_author, :class => 'comment-author')
+  div(:first_comment_text, :class=> 'comment-text')
+  spans(:first_comment_date, :class => 'comment-date-created')
+
+
+  def date_created
+    DateTime.parse(date_element.attribute('title'))
+  end
+
+  def first_comment_date_created
+    DateTime.parse(first_comment_date_elements[0].attribute('title')).strftime(DATETIME_PATTERN)
+  end
+
+  def create_comment
+    @browser.send_keys :tab
+    @browser.send_keys :enter
+  end
+end
